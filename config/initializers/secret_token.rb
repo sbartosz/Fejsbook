d91c961e77c22531abae2da663e69e774b57d35a
Bartosz <bartosz@bartosz-laptop.(none)>
2011-02-25 15:13:41 +0100

    Added Pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitt::Application.config.secret_token = '004241e16d70381c72bab2da9782bd7ea7c5d15f6e787a8f3c495d8102c2778758dd4cc60e530a5adb792056f2c04808f4de09635580752137d5730fac7bc3a7'
