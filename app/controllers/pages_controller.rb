5ec7bd6fa80645c89f7075108c2b454d68d4e77a
Bartosz <bartosz@bartosz-laptop.(none)>
2011-03-05 11:30:55 +0100

    Start

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+	@title = "Strona startowa"	
   end
 
   def contact
+	@title = "Kontakt"
   end
 
   def help
+	@title = "Pomoc"
   end
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+	
+  def new
+	@user = User.new
+	@title = "Zapisz sie"	
+  end
+
+  def index
+    @title = "All users"
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts
+    @title = @user.name
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+       sign_in @user
+       flash[:notice] = "Witamy na facebooku"
+       redirect_to user_path(@user)
+    else
+      @title = "Zarejestruj się"
+      render 'new'
+    end
+  end
+
+  def edit
+	@title = "Edytuj dane osobowe"
+        @user = User.find(params[:id])
+  end
+ 
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Profil zaaktualizowany."
+      redirect_to @user
+    else
+      @title = "Edycja danych"
+      render 'edit'
+    end
+  end	
+
+ private
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+end
+
+
