97b89bc3f6c5ba3f57514bee09b1bbbaa2c78a4a
Bartosz <bartosz@bartosz-laptop.(none)>
2011-03-07 15:46:13 +0100

    fixv2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ActiveRecord::Base
    attr_accessible :content, :title
 
-   validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :content, :presence => true, :length => { :maximum => 140 }
   validates :user_id, :presence => true
 
   belongs_to :user
